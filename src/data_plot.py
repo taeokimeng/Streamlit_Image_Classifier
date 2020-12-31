75aae7592655738737f71ef450dfef3c5edf7d08
Taeo Kim <kweras95@gmail.com>
2020-12-31 10:15:04 +0900

    Add axis title

--- src/data_plot.py
+++ src/data_plot.py
@@ -4,5 +4,7 @@ import streamlit as st
 def plot_bar(selected_model, probability, answer):
     fig = go.Figure(data=[go.Bar(
         x=selected_model, y=probability, text=answer, textposition='inside')])
-    fig.update_layout(title="Classifiers Comparison")
+    fig.update_layout(title="Classifiers Comparison",
+                      xaxis_title="Model",
+                      yaxis_title="Responsibility")
     st.write(fig)
